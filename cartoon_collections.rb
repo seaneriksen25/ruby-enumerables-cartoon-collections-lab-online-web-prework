acde729aae5981e9d265237097ec055b0b1e5e8f
Sean Eriksen <eriksensean@yahoo.com>
2019-10-25 15:48:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,16 +8,12 @@ end
 
 def summon_captain_planet# code an argument here
   
-  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map{|item| "#{item.upcase}!" }
-  summon_captain_planet(planeteer_calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
   
-assorted_words = ["two", "go", "industrious", "bop"]
-long_planeteer_calls(assorted_words)
 
 end
 
@@ -25,12 +21,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
-
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-find_the_cheese(soup)
-
-ingredients = ["garlic", "camembert", "bread"]
-find_the_cheese(ingredients)
+  
 end
